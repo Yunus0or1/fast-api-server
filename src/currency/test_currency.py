e549b38d36c3422e868359f2bd2e5d478ea99842
Yunus0or1 <ahmedyunuspilot@gmail.com>
2023-02-04 16:41:37 +0000

    Added currency basic test

--- src/currency/test_currency.py
+++ src/currency/test_currency.py
@@ -1,1 +1,12 @@
+
 from fastapi.testclient import TestClient
+from src.currency.service import get_app
+
+app = get_app()
+client = TestClient(app)
+headers = {"x-token": "fake-super-secret-token"}
+
+def test_default():
+    response = client.get("/currency/", headers=headers)
+    print(response)
+    assert response.status_code == 200
